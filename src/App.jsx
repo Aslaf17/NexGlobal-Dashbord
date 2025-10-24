c32cb050c28c4e0f9425f707eb6f9ef4784e25d7
Aslaf17 <116107524+Aslaf17@users.noreply.github.com>
2025-10-24 15:38:46 +0530

    INITIAL SETUP

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "./pages/home/Home"
+import Employees from "./pages/employees/Employees"
+import LeaveRequest from "./pages/leave_request/LeaveRequest"
+import Login from "./pages/login/Login"
+import Payroll from "./pages/payroll/Payroll"
+import Reports from "./pages/reports/Reports"
+import Requirement from "./pages/requirement/Requirement"
+
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/employees" element={<Employees />} />
+                    <Route path="/Leaverequest" element={<LeaveRequest />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/payroll" element={<Payroll />} />
+                    <Route path="/reports" element={<Reports />} />
+                    <Route path="/requirement" element={<Requirement />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App
